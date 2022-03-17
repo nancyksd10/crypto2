8c4e137b35e91329ae2a30c91c4e741276557854
Nancy Kasaudhan <66629416+nancyksd10@users.noreply.github.com>
2022-03-17 21:50:55 +0530

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
   'x-bingapis-sdk': 'true',
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-  'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+  'x-rapidapi-key': '294825a399msh9eaf2bfafa5dd22p1ceb78jsn34ae26724c7e',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
 };
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
